de20568700a020734a4d25ffb12389a8c9305e54
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-30 15:25:25 +0100

    updates

--- server/dist/app.js
+++ server/dist/app.js
@@ -11,16 +11,16 @@ const express_1 = __importDefault(require("express"));
 const contactRoute_1 = __importDefault(require("./route/contactRoute"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
 app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 app.use((0, morgan_1.default)("combined"));
 app.use("/", contactRoute_1.default);
 // creating static assets for heroku tp build our file folder
 if (process.env.NODE_ENV === "production") {
     //    create folder
     // express.static delivers static files(html,css). When we want to use build we want to serve this files in the build folder
-    app.use(express_1.default.static(path_1.default.resolve(__dirname, "..", "dists")));
+    app.use(express_1.default.static(path_1.default.resolve(__dirname, "..", "public")));
     // if client makes request we want to send idex.html frist, which in  reality is all the react app
-    app.get("*", (req, res) => res.sendFile(path_1.default.resolve(__dirname, "..", "dists", "index.html")));
+    app.get("*", (req, res) => res.sendFile(path_1.default.resolve(__dirname, "..", "public", "index.html")));
 }
 exports.default = app;
